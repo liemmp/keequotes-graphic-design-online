adbe0e4a151c172ffb59a7353e8f358fe38e6b78
liemmp <79391067+liemmp@users.noreply.github.com>
2021-02-21 11:36:48 +0700

    Update tools.php
    Translate

--- keequotes-graphic-design-online/tool/tools.php
+++ keequotes-graphic-design-online/tool/tools.php
@@ -1,44 +1,44 @@
 <div class="tool-icon">
         <div class="scrollmenu">
             <div class="general-function" style="display:none;">
-              <a title="thu nhỏ" href="javascript:{}" id="zoomout"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/zoomout.svg"></a>
-              <a title="phóng lớn" href="javascript:{}" id="zoomin"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/zoomin.svg"></a>
-              <a title="quay lại" href="javascript:{}" id="undo"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/undo.svg"></a>
-              <a title="làm tiếp" href="javascript:{}" id="redo"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/redo.svg"></a>
-              <a title="đổi kích thước" href="javascript:{}" id="resize"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/resize.svg"></a>
-              <a title="hướng dẫn" href="https://inthiponline.tawk.help/" target="_blank"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/help.svg"></a>
+              <a title="zoom out" href="javascript:{}" id="zoomout"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/zoomout.svg"></a>
+              <a title="zoom in" href="javascript:{}" id="zoomin"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/zoomin.svg"></a>
+              <a title="undo" href="javascript:{}" id="undo"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/undo.svg"></a>
+              <a title="redo" href="javascript:{}" id="redo"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/redo.svg"></a>
+              <a title="resize" href="javascript:{}" id="resize"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/resize.svg"></a>
+              <a title="help" href="https://inthiponline.tawk.help/" target="_blank"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/help.svg"></a>
             <a id="downloader"></a>
             </div>
             <div class="delete-color-function" style="display:none;">
-              <a title="xóa bỏ" href="javascript:{}" id="remove"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/remove.svg"></a>
+              <a title="remove" href="javascript:{}" id="remove"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/remove.svg"></a>
             </div>
             <div class="color-function" style="display:none;">
-              <a title="đổi màu" onclick="create_text_color_solid('#changecolor')" id="color"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/color.svg"></a>
+              <a title="color" onclick="create_text_color_solid('#changecolor')" id="color"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/color.svg"></a>
             </div>
             <div class="text-function" style="display:none;">
-              <a title="thay font" href="javascript:{}" id="textfont"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textfont.svg"></a>
-              <a title="chỉnh chữ" href="javascript:{}" id="textsize"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textsize.svg"></a>
-              <a title="phong cách chữ" href="javascript:{}" id="textstyle"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textstyle.svg"></a>
-              <a title="hiệu ứng chữ" href="javascript:{}" id="texteffects"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/texteffects.svg"></a>
-              <a title="canh dòng" href="javascript:{}" id="textalign"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textalign.svg"></a>
+              <a title="font" href="javascript:{}" id="textfont"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textfont.svg"></a>
+              <a title="text property" href="javascript:{}" id="textsize"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textsize.svg"></a>
+              <a title="text style" href="javascript:{}" id="textstyle"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textstyle.svg"></a>
+              <a title="text effect" href="javascript:{}" id="texteffects"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/texteffects.svg"></a>
+              <a title="text align" href="javascript:{}" id="textalign"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/textalign.svg"></a>
             </div>
             <div class="image-function" style="display:none;">
-              <a title="dùng làm nền" href="javascript:{}" id="makeitbackground"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/makeitbackground.svg"></a>
+              <a title="make it background" href="javascript:{}" id="makeitbackground"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/makeitbackground.svg"></a>
             </div>
             <div class="group-function" style="display:none;">
-              <a title="gộp nhóm" href="javascript:{}" id="group"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/group.svg"></a>
-              <a title="rã nhóm" href="javascript:{}" id="ungroup"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/ungroup.svg"></a>
-              <a title="lồng ghép" href="javascript:{}" id="createmark"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/mask.svg"></a>
+              <a title="group" href="javascript:{}" id="group"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/group.svg"></a>
+              <a title="ungroup" href="javascript:{}" id="ungroup"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/ungroup.svg"></a>
+              <a title="combine" href="javascript:{}" id="createmark"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/mask.svg"></a>
             </div>
             <div class="detail-function" style="display:none;">
-              <a title="canh chỉnh vị trí" href="javascript:{}" id="align"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/acenter.svg"></a>
-              <a title="di chuyển lớp" href="javascript:{}" id="arrange"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/top.svg"></a>
-              <a title="lật ngang" href="javascript:{}" id="flipX"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/vflip.svg"></a>
-              <a title="lật dọc" href="javascript:{}" id="flipY"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/hflip.svg"></a>
-              <a title="độ trong suốt" href="javascript:{}" id="opacity"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/opacity.svg"></a>
-              <a title="nhân đôi" href="javascript:{}" id="clone"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/clone.svg"></a>
-              <a title="mở khóa" href="javascript:{}" id="unlock"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/unlock.svg"></a>
-              <a title="khóa" href="javascript:{}" id="lock"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/lock.svg"></a>
+              <a title="align" href="javascript:{}" id="align"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/acenter.svg"></a>
+              <a title="arrange" href="javascript:{}" id="arrange"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/top.svg"></a>
+              <a title="flipx" href="javascript:{}" id="flipX"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/vflip.svg"></a>
+              <a title="flipy" href="javascript:{}" id="flipY"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/hflip.svg"></a>
+              <a title="opacity" href="javascript:{}" id="opacity"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/opacity.svg"></a>
+              <a title="clone" href="javascript:{}" id="clone"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/clone.svg"></a>
+              <a title="unlock" href="javascript:{}" id="unlock"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/unlock.svg"></a>
+              <a title="lock" href="javascript:{}" id="lock"><img src="<?php echo KGDO_CD_URL; ?>/images/tools/lock.svg"></a>
             </div>
         </div>
 </div>
