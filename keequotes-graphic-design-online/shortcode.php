0103b77cb6399e68a2081beee9545c5ca54f9b74
liemmp <79391067+liemmp@users.noreply.github.com>
2021-02-21 12:09:04 +0700

    Update shortcode.php

--- keequotes-graphic-design-online/shortcode.php
+++ keequotes-graphic-design-online/shortcode.php
@@ -1,10 +1,11 @@
 <?php
+
 do_action('CD_enqueue_scripts');
 $check = check_license_plugin(KGDO_K_API);
 $date = date('Y-m-d');
 $li = $check->license->ended_at;
 if($li<$date){
-
+echo '<div>Welcome back!<br/> <a href="admin.php?page=setting_plugin">Log in</a> with your account on Setting and use the Editor to edit Templates here.';
 }
 ?>
 <div class="display-products content" style="min-height: 100vh; position: relative; z-index: 9; background-color: #fff">
@@ -131,4 +132,3 @@ if($li<$date){
        'img_src': '<?php echo KGDO_CD_URL.'/images/8.jpg' ?>',
     };
 </script>
-
