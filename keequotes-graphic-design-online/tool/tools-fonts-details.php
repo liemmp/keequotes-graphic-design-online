bd0deeec395422c68cdbf5d4ad8deb51534ec6bb
liemmp <79391067+liemmp@users.noreply.github.com>
2021-02-21 11:35:23 +0700

    Update tools-fonts-details.php
    Translate

--- keequotes-graphic-design-online/tool/tools-fonts-details.php
+++ keequotes-graphic-design-online/tool/tools-fonts-details.php
@@ -1,12 +1,12 @@
 <div class="tool-icon-font">
     <div class="scrollmenu-font" style="overflow:auto">
-        <p>Kích thước chữ</p>
+        <p>Font size</p>
         <input type="range" min="12" max="112" value="22" class="rangeslider" id="txtFonts" style="width: 70%;"/><input type="text" id="demotxtFonts" size="2" style="border: 0; width: 21px;"><p></p>
-        <p>Độ dày chữ</p>
+        <p>Font weight</p>
         <input type="range" min="100" max="900" value="300" step="100" class="rangeslider" id="txtFontw" style="width: 70%;"/><input type="text" id="demotxtFontw" size="2" style="border: 0; width: 21px;"><p></p>
-        <p>Chiều cao dòng</p>
+        <p>Line height</p>
         <input type="range" min="-3.0" max="5.0" value="1" step="0.1" class="rangeslider" id="txtFontlh" style="width: 70%;"/><input type="text" id="demotxtFontlh" size="2" style="border: 0; width: 21px;"><p></p>
-        <p>Khoảng cách chữ</p>
+        <p>Font spacing</p>
         <input type="range" min="-300" max="1000" value="100" step="50" class="rangeslider" id="txtFontchs" style="width: 70%;"/><input type="text" id="demotxtFontchs" size="2" style="border: 0; width: 21px;">
         <p></p>
     </div>
